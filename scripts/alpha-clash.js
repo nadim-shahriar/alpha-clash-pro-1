55f298856b459aa3829a859719eb9657451ed05e
nadim-shahriar <swajan321@gmail.com>
2024-10-03 20:13:56 +0600

    Show Element

---
+++ scripts/alpha-clash.js
@@ -0,0 +1,15 @@
+// function play() {
+//     const homeSection = document.getElementById('home-screen')
+//     homeSection.classList.add('hidden')
+//     // console.log(homeSection.classList)
+
+//     // show the playground
+//     const playgroundSection = document.getElementById('play-ground')
+//     playgroundSection.classList.remove('hidden')
+//     // console.log(playgroundSection.classList)
+
+// }
+function play() {
+    hideElementById('home-screen')
+    showElementById('play-ground')
+}
